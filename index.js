84cb7b7e9f865e19d3a1da3d7dc5e4d5be327329
keeks5456 <Akikogreen08@gmail.com>
2020-08-29 19:21:33 -0700

    everything done besides edit function

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-const test = document.querySelector('h2')
-test.style.color = 'blue'
-console.log(test)
+const taskCard = document.querySelector('.task-card') //task card in container
+const form = document.querySelector('#create-task-form')
+const ul = document.getElementById('tasks')
+
+
+const listenForSubmit = () => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newTask = document.getElementById('description').value
+    const li = document.createElement('li')
+    li.innerHTML += `
+      ${newTask}
+      <button class="btn-danger" data-delete="delete">Delete</button>
+      <button class="btn-success" data-edit="edit">Edit</button> 
+      <hr>
+      `
+    ul.appendChild(li)
+    form.reset()
+
+  }) 
+}//end of submit function
+
+listenForSubmit()
+
+const listenForEditOrDelete = () => {
+  ul.addEventListener('click', (e) => {
+    if(e.target.dataset.delete === 'delete'){
+      e.target.parentNode.remove()
+    } else if(e.target.dataset.edit === 'edit'){
+      form[0].value = e.target.parentNode.innerText.split(' ')[0]
+      form[1].value = 'Edit'
+      form.addEventListener('submit', (e) => {
+        console.log('edit')
+      })
+      
+    }
+    // debugger
+  })
+}
+listenForEditOrDelete()
+
+
+const listenForCreateOrEdit = () => {
+  if(form[1].innerHTML = "Edit"){
+    listenForEditOrDelete
+  } else {
+    form[1].innerHTML = "Create New Task"
+    listenForSubmit
+  }
+}
+listenForCreateOrEdit()
